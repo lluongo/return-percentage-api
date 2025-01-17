511c32986bef34a9884fe85e810d7304d00dedec
lluongo <lucas.luongo@xepelin.com>
2025-01-17 12:41:41 -0300

    cambio de path

--- src/main/java/com/tenpo/return_percentage_api/presentation/PercentageController.java
+++ src/main/java/com/tenpo/return_percentage_api/presentation/PercentageController.java
@@ -1,14 +1,14 @@
 package com.tenpo.return_percentage_api.presentation;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
+@RequestMapping("/v1")
 public class PercentageController {
 
-    @GetMapping("percentage")
+    @GetMapping("/percentage")
     public Double getPercentage() {
         return 100.0;
     }
